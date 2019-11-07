013f2ee2ca54d1d206a88a311847ededcd1cd1d7
SashaZaicev <sasha167@bk.ru>
2019-11-07 19:06:58 +0300

    Updates

---
+++ precache-manifest.b48bce748510f9920dd4791f8f338924.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd014893cd16489d352c634d5650b39",
+    "url": "/social_network_zaicev/index.html"
+  },
+  {
+    "revision": "073814294bedf00723f7",
+    "url": "/social_network_zaicev/static/css/main.d15d03ef.chunk.css"
+  },
+  {
+    "revision": "bfacb4e63a2275ae06e2",
+    "url": "/social_network_zaicev/static/js/2.a7a20851.chunk.js"
+  },
+  {
+    "revision": "073814294bedf00723f7",
+    "url": "/social_network_zaicev/static/js/main.6f99640d.chunk.js"
+  },
+  {
+    "revision": "02c726618b8c482c62dd",
+    "url": "/social_network_zaicev/static/js/runtime~main.2e2e20a0.js"
+  },
+  {
+    "revision": "e3873dc10a343b99d7dbf3d6779ea8bc",
+    "url": "/social_network_zaicev/static/media/1.e3873dc1.png"
+  },
+  {
+    "revision": "6d6f56e6ac9195d44d68ed4ec9a7d79a",
+    "url": "/social_network_zaicev/static/media/2.6d6f56e6.png"
+  },
+  {
+    "revision": "896435c49fc143b9404080cc3bc533d9",
+    "url": "/social_network_zaicev/static/media/2mashi.896435c4.mp3"
+  },
+  {
+    "revision": "f9ee43dca2d59843561a57c94d03828b",
+    "url": "/social_network_zaicev/static/media/3.f9ee43dc.png"
+  },
+  {
+    "revision": "79fcaf2d504979dd27a860599899a33d",
+    "url": "/social_network_zaicev/static/media/artur.79fcaf2d.mp3"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/social_network_zaicev/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9ea0fc60bb29a508a8bac214ec83771e",
+    "url": "/social_network_zaicev/static/media/preloader.9ea0fc60.svg"
+  },
+  {
+    "revision": "2c541d5295263266463d3a0479a43e6b",
+    "url": "/social_network_zaicev/static/media/tima.2c541d52.mp3"
+  }
+]);
